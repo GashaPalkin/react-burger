367d120ca3704221f82c0da7d991215b2c433ab2
GashaPalkin <pavelgalkin.mgn@gmail.com>
2023-03-31 20:30:50 +0500

    sprint 6 fixed

--- cypress/e2e/testDND.spec.cy.ts
+++ cypress/e2e/testDND.spec.cy.ts
@@ -1,30 +1,39 @@
+import { testUrl } from '../../src/utils/constants'
+import { testIngredientsEl } from '../../src/utils/constants'
+import { testdropContainerEl } from '../../src/utils/constants'
+import { testdropIngredientsEl } from '../../src/utils/constants'
+import { testBun } from '../../src/utils/constants'
+import { testSauce } from '../../src/utils/constants'
+import { testMainIngredient } from '../../src/utils/constants'
+
+
 describe('Testing DnD', function () {
 
   beforeEach(function () {
     cy.intercept('GET', 'ingredients', { fixture: 'fakeIngredients' })
-    cy.visit('http://localhost:3000');
+    cy.visit(testUrl);
   });
 
   it('should contain ingredients', function () {
-    cy.contains('Краторная булка N-200i').should('exist');
-    cy.contains('Соус Spicy-X').should('exist');
-    cy.contains('Мясо бессмертных моллюсков Protostomia').should('exist');
+    cy.contains(testBun).should('exist');
+    cy.contains(testSauce).should('exist');
+    cy.contains(testMainIngredient).should('exist');
   });
 
   it('should drag and drop bun', function () {
-    cy.get('[data-testid=ingredients]').contains('Краторная булка N-200i').trigger('dragstart');
-    cy.get('[data-testid=dropContainer]').trigger('drop');
-    cy.get('[data-testid=dropBunTop]').contains('Краторная булка N-200i').should('exist');
-    cy.get('[data-testid=dropBunBottom]').contains('Краторная булка N-200i').should('exist');
+    cy.get(testIngredientsEl).contains(testBun).trigger('dragstart');
+    cy.get(testdropContainerEl).trigger('drop');
+    cy.get('[data-testid=dropBunTop]').contains(testBun).should('exist');
+    cy.get('[data-testid=dropBunBottom]').contains(testBun).should('exist');
   });
 
   it('should drag and drop ingredient', function () {
-    cy.get('[data-testid=ingredients]').contains('Соус Spicy-X').trigger('dragstart');
-    cy.get('[data-testid=dropContainer]').trigger('drop');
-    cy.get('[data-testid=dropIngredients]').contains('Соус Spicy-X').should('exist');
-    cy.get('[data-testid=ingredients]').contains('Мясо бессмертных моллюсков Protostomia').trigger('dragstart');
-    cy.get('[data-testid=dropContainer]').trigger('drop');
-    cy.get('[data-testid=dropIngredients]').contains('Мясо бессмертных моллюсков Protostomia').should('exist');
+    cy.get(testIngredientsEl).contains(testSauce).trigger('dragstart');
+    cy.get(testdropContainerEl).trigger('drop');
+    cy.get(testdropIngredientsEl).contains(testSauce).should('exist');
+    cy.get(testIngredientsEl).contains(testMainIngredient).trigger('dragstart');
+    cy.get(testdropContainerEl).trigger('drop');
+    cy.get(testdropIngredientsEl).contains(testMainIngredient).should('exist');
   });
 
 }); 
